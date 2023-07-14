6b9d4f5618d94bec96762ca34274946c5b73ac2d
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2023-07-14 15:50:03 -0300

    aprendendo "list  comprehension"

---
+++ listcomprehensionmapa.py
@@ -0,0 +1,12 @@
+produtos = [
+    {'nome':'p1', 'preço':10,},
+    {'nome':'p2', 'preço':20},
+    {'nome':'p2', 'preço':30}
+]
+
+novo_preco = [
+    {**produto, 'preço':produto['preço']*1.05}
+    if produto['preço']>20 else {**produto}
+    for produto in produtos
+]
+print(*novo_preco,sep='\n')
