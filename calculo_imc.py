a5925600b231b93fd9abd141af493ff26dd3d2bf
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2023-07-17 07:48:47 -0300

    Calculadora IMC

---
+++ calculo_imc.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
