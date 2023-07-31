5bed6115c95edd335bac21fd1c40c57d0b9c4d7a
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2023-07-31 19:31:47 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,13 +1,4 @@
-#     'nome': 'Luiz Otávio',
-#     'sobrenome': 'Miranda',
-#     'idade': 18,
-#     'altura': 1.8,
-#     'endereços': [
-#         {'rua': 'tal tal', 'número': 123},
-#         {'rua': 'outra rua', 'número': 321},
-#     ]
-# }
-# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
+
 pessoa = {
     'nome': 'Eduardo',
     'sobrenome': 'Costa',
