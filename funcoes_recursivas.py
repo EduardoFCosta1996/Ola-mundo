98d7ee257b3badf023ef15fdaae9feeaf960b804
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2023-08-07 19:08:00 -0300

    Funções recursivas

---
+++ funcoes_recursivas.py
@@ -0,0 +1,15 @@
+def recursiva(inicio=0, fim=4):
+
+    print(inicio, fim)
+
+    # Caso base
+    if inicio >= fim:
+        return fim
+
+    # Caso recursivo
+    # contar até chegar ao final
+    inicio += 1
+    return recursiva(inicio, fim)
+
+
+print(recursiva())
