0da8d36651e976a8a99d8f6d5347d9dbccc55462
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2023-10-09 20:05:09 -0300

    @property @setter

---
+++ @property.py
@@ -0,0 +1,44 @@
+class Caneta:
+    def __init__(self, cor):
+        self.cor_tinta = cor
+
+    @property
+    def cor(self):
+        print('PROPERTY')
+        return self.cor_tinta
+
+    @property
+    def cor_tampa(self):
+        return 123456
+
+###########################
+
+
+caneta = Caneta('Azul')
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor_tampa)
+
+#####################################
+
+# class Caneta:
+#     def __init__(self, cor):
+#         self.cor_tinta = cor
+
+#     def get_cor(self):
+#         print('GET COR')
+#         return self.cor_tinta
+
+# ###########################
+
+
+# caneta = Caneta('Azul')
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
