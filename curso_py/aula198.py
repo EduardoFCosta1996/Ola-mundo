532dad70864bb0c3aeabe880118c392f9af1ce8f
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2024-09-05 21:06:07 -0300

    05/09/24

---
+++ curso_py/aula198.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Eduardo'
+p1.sobrenome= 'Costa'
+
+p2 = Pessoa()
+p2.nome = 'Amora'
+p2.sobrenome = 'Nala'
+print(p1.nome)
+print(p1.sobrenome)
+print(p2.nome)
+print(p2.sobrenome)
