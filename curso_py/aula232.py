7b36d7d36874d238748ed00d473c978fdae1cf30
EduardoFCosta1996 <124738239+EduardoFCosta1996@users.noreply.github.com>
2024-09-23 21:09:43 -0300

    23/09/24

---
+++ curso_py/aula232.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, menssagem):
+        self.messagem = menssagem
+        
+    @abstractmethod
+    def enviar(self): ...
+               
+class NotificacaoEmail(Notificacao) -> boll:
+    def enviar(self):
+        print('email: enviando', self.messagem)
+        return True
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self):
+        print('SMS: enviando', self.messagem)
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+    if notificacao_enviada:
+        print('notificacao enviada')
+    else:
+        print('notificacao nao enviada')
+
+notificacao_email = NotificacaoEmail('email enviado')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('testando sms')
+notificar(notificacao_sms)
